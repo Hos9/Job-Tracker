84af784e6ea1ebd841082e0dfa863e2c1759950b
Hos9 <shahin9315@gmail.com>
2026-02-22 19:10:54 +0600

    Buttons response & machine.js added

--- scripts/machine.js
+++ scripts/machine.js
@@ -1,21 +1,11 @@
 function showOnly(id) {
-  const cardOne = document.getElementById("job-card-1");
-  const cardTwo = document.getElementById("job-card-2");
-  const cardThree = document.getElementById("job-card-3");
-  const cardFour = document.getElementById("job-card-4");
-  const cardFive = document.getElementById("job-card-5");
-  const cardSix = document.getElementById("job-card-6");
-  const cardSeven = document.getElementById("job-card-7");
-  const cardEight = document.getElementById("job-card-8");
+  const allJobs = document.getElementById("all-jobs");
+  const interview = document.getElementById("interview");
+  const rejected = document.getElementById("rejected");
 
-  cardOne.classList.add("hidden");
-  cardTwo.classList.add("hidden");
-  cardThree.classList.add("hidden");
-  cardFour.classList.add("hidden");
-  cardFive.classList.add("hidden");
-  cardSix.classList.add("hidden");
-  cardSeven.classList.add("hidden");
-  cardEight.classList.add("hidden");
+  allJobs.classList.add("hidden");
+  interview.classList.add("hidden");
+  rejected.classList.add("hidden");
 
   const selected = document.getElementById(id);
   selected.classList.remove("hidden");
