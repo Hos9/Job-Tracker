4b00ba047f6ee8be7448fd67965534b8f3b33015
Hos9 <shahin9315@gmail.com>
2026-02-24 07:28:47 +0600

    Child append in interview section succesfully

--- scripts/machine.js
+++ scripts/machine.js
@@ -1,11 +1,11 @@
 // Delete Button
 document.addEventListener("click", function (e) {
-  const isDeleteBtn = e.target.closest('button[data-action="delete-card"]');
-  if (isDeleteBtn == false) {
+  const btn = e.target.closest('button[data-action="delete-card"]');
+  if (btn == false) {
     return;
   }
 
-  const card = isDeleteBtn.closest(".job-card");
+  const card = btn.closest(".job-card");
   if (card == false) {
     return;
   }
@@ -23,6 +23,8 @@ let rejectedCount = document.getElementById("rejectedCount");
 let totalJobsCount = document.getElementById("total-jobs");
 
 const allJobsSection = document.getElementById("all-jobs");
+const interviewSection = document.getElementById("interview");
+const rejectedSection = document.getElementById("rejected");
 const mainContainer = document.querySelector("main");
 
 function calculateCount() {
@@ -67,5 +69,100 @@ function toggleStyle(id) {
     rejected.classList.add("hidden");
     interview.classList.add("hidden");
     allJobs.classList.remove("hidden");
+  }
+}
+
+mainContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("btn-interview")) {
+    const parentNode = event.target.parentNode.parentNode.parentNode;
+
+    const jobTitle = parentNode.querySelector(".job-title").innerText;
+    const jobDetails = parentNode.querySelector(".job-details").innerText;
+    const salaryRange = parentNode.querySelector(".salary-range").innerText;
+    const status = parentNode.querySelector(".status").innerText;
+    const toDo = parentNode.querySelector(".to-do").innerText;
+
+    parentNode.querySelector(".status").innerText = "INTERVIEW";
+    parentNode
+      .querySelector(".status")
+      .classList.add("border-green-500", "text-green-500");
+
+    const cardInfo = {
+      jobTitle,
+      jobDetails,
+      salaryRange,
+      status,
+      toDo,
+    };
+
+    const jobTitleExist = interviewList.find(
+      (item) => item.jobTitle == cardInfo.jobTitle,
+    );
+
+    if (!jobTitleExist) {
+      interviewList.push(cardInfo);
+    }
+    renderInterview();
+  }
+});
+
+function renderInterview() {
+  interviewSection.innerHTML = "";
+
+  for (let interview of interviewList) {
+    console.log(interview);
+
+    let div = document.createElement("div");
+    // div.className = ''
+    div.innerHTML = `
+    <div
+          id="job-card-1"
+          class="job-card relative space-y-5 bg-base-200 p-6 shadow-sm rounded-lg mb-4"
+        >
+          <div class="card">
+            <h1 class="job-title font-bold text-xl">Mobile First Corp</h1>
+            <p class="job-details">React Native Developer</p>
+            <button
+              type="button"
+              class="dlt-btn btn btn-circle btn-soft btn-error border-gray-300 btn-lg absolute right-3 top-0 z-50"
+              aria-label="Delete job card"
+              data-action="delete-card"
+            >
+              <i class="fa-solid fa-trash"></i>
+            </button>
+          </div>
+          <div class="card">
+            <p class="salary-range">Remote • Full-time • $130,000 - $175,000</p>
+          </div>
+          <div class="card">
+            <div class="status btn btn-soft w-36 mb-2 border-gray-500">
+              Not Applied
+            </div>
+            <p class="to-do">
+              Build cross-platform mobile applications using React Native. Work
+              on products used by millions of users worldwide.
+            </p>
+          </div>
+          <div class="card felx md:flex-row gap-5">
+            <div>
+              <button id="btn-interview"
+                class="btn btn-soft btn-success w-30  border-green-600 text-green-600"
+              >
+                INTERVIEW
+              </button>
+            </div>
+            <div>
+              <button id="btn-rejected"
+                class="btn btn-soft btn-error w-30  border-red-600 text-red-600"
+              >
+                REJECTED
+              </button>
+            </div>
+          </div>
+        </div>
+    
+    `;
+
+    interviewSection.appendChild(div);
   }
 }
